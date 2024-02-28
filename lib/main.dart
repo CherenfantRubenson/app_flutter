6263fc456509766af1fb3e9eeb0981d7565b7b74
Rubenson Cherenfant <92955778+CherenfantRubenson@users.noreply.github.com>
2024-02-28 16:21:28 -0500

    Prod

--- lib/main.dart
+++ lib/main.dart
@@ -31,10 +31,10 @@ class MyApp extends StatelessWidget {
               },
             ),
             ListTile(
-              title: const Text('Lis Prodwi'),
+              title: const Text('List Pwodwi'),
               onTap: () {
                 Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => LisPwodwi()),
+                  MaterialPageRoute(builder: (context) => ListPwodwiPage()),
                 );
               },
             ),
@@ -49,146 +49,204 @@ class MyApp extends StatelessWidget {
         ),
       ),
       body: Column(
-        children: <Widget>[
-          // Premier Row
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                  height: 100,
-                  color: Colors.blue,
-                  child: Center(
-                    child: Text(
-                      'Premier Row',
-                      style: TextStyle(color: Colors.white),
-                    ),
+  children: <Widget>[
+    // Premier Row
+    Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            height: 100,
+            color: Colors.blue,
+            child: Center(
+              child: Text(
+                'Premier Row',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+    SizedBox(height: 20), // Espace entre les deux Row
+    // Deuxième Row
+    Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            height: 100,
+            color: Colors.blue,
+            child: Center(
+              child: Text(
+                'Deuxième Row',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+      ],
+    ),
+    SizedBox(height: 20), // Espace entre les deux Row
+    // Les carrés qui occupent la moitié de l'écran
+    Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(8.0),
+                width: MediaQuery.of(context).size.width / 2 - 20, // Moitié de la largeur de l'écran avec un espace entre les deux
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 9, 39, 64),
+                  border: Border.all(
+                    color: Color.fromARGB(255, 9, 39, 64),
+                    width: 10.0,
                   ),
                 ),
               ),
+              SizedBox(height: 8),
+              Text(
+                'Savon',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                style: TextStyle(fontSize: 12),
+              ),
+              SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.shopping_cart),
+                  SizedBox(width: 8),
+                  Icon(Icons.favorite),
+                ],
+              ),
             ],
           ),
-          SizedBox(height: 20), // Espace entre les deux Row
-          // Deuxième Row
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Expanded(
-                child: Container(
-                  height: 100,
-                  color: Colors.blue,
-                  child: Center(
-                    child: Text(
-                      'Deuxième Row',
-                      style: TextStyle(color: Colors.white),
-                    ),
+        ),
+        SizedBox(width: 20), // Espace entre les deux carrés
+        Expanded(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(8.0),
+                width: MediaQuery.of(context).size.width / 2 - 20, // Moitié de la largeur de l'écran avec un espace entre les deux
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 9, 39, 64),
+                  border: Border.all(
+                    color: Color.fromARGB(255, 9, 39, 64),
+                    width: 10.0,
                   ),
                 ),
               ),
-            ],
-          ),
-          SizedBox(height: 20), // Espace entre les deux Row
-          // Les carrés qui occupent la moitié de l'écran
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Expanded(
-                child: Column(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.all(8.0),
-                      width: MediaQuery.of(context).size.width / 2 - 20, // Moitié de la largeur de l'écran avec un espace entre les deux
-                      height: 100,
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 9, 39, 64),
-                        border: Border.all(
-                          color: Color.fromARGB(255, 9, 39, 64),
-                          width: 10.0,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Savon',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                      style: TextStyle(fontSize: 12),
-                    ),
-                    SizedBox(height: 8),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.shopping_cart),
-                        SizedBox(width: 8),
-                        Icon(Icons.favorite),
-                      ],
-                    ),
-                  ],
-                ),
+              SizedBox(height: 8),
+              Text(
+                'Savon',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
-              SizedBox(width: 20), // Espace entre les deux carrés
-              Expanded(
-                child: Column(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.all(8.0),
-                      width: MediaQuery.of(context).size.width / 2 - 20, // Moitié de la largeur de l'écran avec un espace entre les deux
-                      height: 100,
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 9, 39, 64),
-                        border: Border.all(
-                          color: Color.fromARGB(255, 9, 39, 64),
-                          width: 10.0,
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Savon',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(height: 8),
-                    Text(
-                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-                      style: TextStyle(fontSize: 12),
-                    ),
-                    SizedBox(height: 8),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.shopping_cart),
-                        SizedBox(width: 8),
-                        Icon(Icons.favorite),
-                      ],
-                    ),
-                  ],
-                ),
+              SizedBox(height: 8),
+              Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                style: TextStyle(fontSize: 12),
+              ),
+              SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.shopping_cart),
+                  SizedBox(width: 8),
+                  Icon(Icons.favorite),
+                ],
               ),
             ],
           ),
-        ],
+        ),
+      ],
+    ),
+  ],
+),
+
+    );
+  }
+}
+
+class ListPwodwiPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('List Pwodwi'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(8, (index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => DetailPage(productIndex: index)),
+              );
+            },
+            child: Card(
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(8.0),
+                    width: double.infinity,
+                    height: 100,
+                    color: Color.fromARGB(255, 9, 39, 64),
+                    child: Center(
+                      child: Text(
+                        '',
+                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Savon',
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  SizedBox(height: 8),
+                ],
+              ),
+            ),
+          );
+        }),
       ),
     );
   }
 }
 
-class LisPwodwi extends StatefulWidget {
-  @override
-  LispwodwiState createState() => LispwodwiState();
-}
+class DetailPage extends StatelessWidget {
+  final int productIndex;
 
-class LispwodwiState extends State<LisPwodwi> {
+  DetailPage({required this.productIndex});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Lis PWodwi'),
+        title: Text('Detay'),
       ),
       body: Center(
-        child: Text('Paje Pwodwi'),
+        child: Text('Detay podwi'),
       ),
     );
   }
